2afd97a77566bcdfa1c776fabe333ba5ccf092e2
carlostiguaque <c.tiguaque@uniandes.edu.co>
2023-11-16 13:01:46 -0500

    Se ajusta Resemble

--- Resemble/index.js
+++ Resemble/index.js
@@ -3,28 +3,37 @@ const compareImages = require("resemblejs/compareImages")
 const config = require("./config.json");
 const fs = require('fs');
 const path = require('path');
+const GhostIni = config.urlIni;
+const GhostFin = config.urlFin;
+const carpetas = config.carpetas;
+let datetime = new Date().toISOString().replace(/:/g, ".");
+
 
 const { viewportHeight, viewportWidth, browsers, options } = config;
 
 async function executeTest() {
-  if (browsers.length === 0) {
-    return;
-  }
   let resultInfo = {}
-  let datetime = new Date().toISOString().replace(/:/g, ".");
-  const firstFolder = path.join(__dirname, '../GHOST_5_68_0/Kraken/reports/');
-  const secondFolder = path.join(__dirname, '../GHOST_4_44_0/Kraken/reports/');
+  const respuesta = {};
+  const firstFolder = path.join(__dirname, GhostIni);
+  const secondFolder = path.join(__dirname, GhostFin);
 
   if (!fs.existsSync(`./results/${datetime}`)) {
     fs.mkdirSync(`./results/${datetime}`, { recursive: true });
   }
+  let bandera = 0;
   const filesF = await fs.readdirSync(firstFolder);
+
   for (const folder of filesF) {
-
-
-  if (!fs.existsSync(`./results/${datetime}/${folder}`)) {
-    fs.mkdirSync(`./results/${datetime}/${folder}`, { recursive: true });
+    if (!carpetas.includes(folder)) {
+      await fs.rmdirSync(path.join(firstFolder, folder), { force: true });
+    }
   }
+
+  for (const folder of filesF) {
+    bandera = bandera + 1;
+    if (!fs.existsSync(`./results/${datetime}/${folder}`)) {
+      fs.mkdirSync(`./results/${datetime}/${folder}`, { recursive: true });
+    }
     const files1 = await fs.readdirSync(firstFolder + "/" + folder);
     // Recorrer cada archivo en la primera carpeta
     for (const file of files1) {
@@ -40,7 +49,7 @@ async function executeTest() {
       // ...
 
 
-      resultInfo['Comparación'] = {
+      resultInfo[file] = {
         isSameDimensions: data.isSameDimensions,
         dimensionDifference: data.dimensionDifference,
         rawMisMatchPercentage: data.rawMisMatchPercentage,
@@ -49,58 +58,78 @@ async function executeTest() {
         analysisTime: data.analysisTime
       }
       fs.writeFileSync(`./results/${datetime}/${folder}/compare-${file}`, data.getBuffer());
+
     }
+    respuesta[folder] = resultInfo;
+    resultInfo = {}
   }
-
-  fs.writeFileSync(`./results/${datetime}/report.html`, createReport(datetime, resultInfo));
-  fs.copyFileSync('./index.css', `./results/${datetime}/index.css`);
-
-  console.log('------------------------------------------------------------------------------------')
-  console.log("Execution finished. Check the report under the results folder")
-  return resultInfo;
+  return respuesta;
 }
 (async () => console.log(await executeTest()))();
 
-function browser(b, info) {
-  return `<div class=" browser" id="test0">
-    <div class=" btitle">
-        <h2>Browser: ${b}</h2>
-        <p>Data: ${JSON.stringify(info)}</p>
-    </div>
-    <div class="imgline">
-      <div class="imgcontainer">
-        <span class="imgname">Reference</span>
-        <img class="img2" src="before-${b}.png" id="refImage" label="Reference">
+
+
+function escenarios(escenario, respuesta) {
+  return `
+  <h2>${escenario}</h2>
+  ${Object.keys(respuesta[escenario]).map((imagen) => {
+    return `
+    <div class="browser" id="test0">
+      <div class=" btitle">
+          <h3>Caso de prueba: ${imagen}</h3>
       </div>
-      <div class="imgcontainer">
-        <span class="imgname">Test</span>
-        <img class="img2" src="after-${b}.png" id="testImage" label="Test">
+      <div class="imgline">
+        <div class="imgcontainer">
+          <span class="imgname">Referencia ${GhostIni}</span>
+          <img class="img2" src="../../${GhostIni}/${escenario}/${imagen}" id="refImage" label="Reference">
+        </div>
+        <div class="imgcontainer">
+          <span class="imgname">Test ${GhostFin}</span>
+          <img class="img2" src="../../${GhostFin}/${escenario}/${imagen}" id="testImage" label="Test">
+        </div>
+      </div>
+      <div class="imgline">
+        <div class="imgcontainer">
+          <span class="imgname">Resultado comparación</span>
+          <img class="imgfull" src="./${escenario}/compare-${imagen}" id="diffImage" label="Diff">
+        </div>
       </div>
     </div>
-    <div class="imgline">
-      <div class="imgcontainer">
-        <span class="imgname">Diff</span>
-        <img class="imgfull" src="./compare-${b}.png" id="diffImage" label="Diff">
+    `;
+  })}
+  `;
+}
+function createReport(datetime, respuesta) {
+  return `
+  <html>
+    <head>
+      <title> VRT Report </title>
+      <link href="index.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+      <h1>Reporte para 
+        <a href="../../${GhostIni}"> ${GhostIni}</a>
+        <a href="../../${GhostFin}"> ${GhostFin}</a>
+      </h1>
+      <p>Executado: ${datetime}</p>
+      <div id="visualizer"> Escenario de Prueba: 
+        ${Object.keys(respuesta).map((escenario) => {
+    return escenarios(escenario, respuesta);
+  })}
       </div>
-    </div>
-  </div>`
+    </body>
+  </html>
+  `;
 }
 
-function createReport(datetime, resInfo) {
-  return `
-    <html>
-        <head>
-            <title> VRT Report </title>
-            <link href="index.css" type="text/css" rel="stylesheet">
-        </head>
-        <body>
-            <h1>Report for 
-                 <a href="${config.url}"> ${config.url}</a>
-            </h1>
-            <p>Executed: ${datetime}</p>
-            <div id="visualizer">
-                ${config.browsers.map(b => browser(b, resInfo[b]))}
-            </div>
-        </body>
-    </html>`
+async function main() {
+  const respuesta = await executeTest();
+
+  fs.writeFileSync(`./results/${datetime}/report.html`, createReport(datetime, respuesta));
+  fs.copyFileSync('./index.css', `./results/${datetime}/index.css`);
+  //fs.cpSync("./screenshots", `../docs/screenshots`, { recursive: true });
+
+  console.log("-------------------------------------------------------------");
+  console.log("Execution finished. Check the report under the results folder");
 }
+main();
