0fe7239a4fb109f730edd6ac3a4f0511c5ed31ce
Juan Camilo <j.villadap@uniandes.edu.co>
2023-11-17 16:22:59 -0500

    Se ponen screenshots a los scripts de ghost 4.44 en playwright

--- GHOST_4_44_0/playwright_ts/tests/posts.spec.ts
+++ GHOST_4_44_0/playwright_ts/tests/posts.spec.ts
@@ -8,33 +8,43 @@ test.describe.serial("Posts E2E Scenarios", () => {
         const user: string = 'j.villadap@uniandes.edu.co';
         const password: string =  'hola123456';
         await page.goto('http://localhost:3002/ghost/#/');
-
+        await page.screenshot({path: './screenshots/post/login/step1.png'})
         await page.getByPlaceholder('jamie@example.com').fill(user);
+        await page.screenshot({path: './screenshots/post/login/step2.png'})
         await page.getByPlaceholder('•••••••••••••••').fill(password);
+        await page.screenshot({path: './screenshots/post/login/step3.png'})
         await page.getByRole('button', { name: 'Sign in →', exact: true}).click();
-        
+        await page.screenshot({path: './screenshots/post/login/step4.png'})
         // Expect the url "to contain" a substring.
         await expect(page.getByTitle('Dashboard')).toBeVisible();
         console.log("----------Login successful----------");
-        await page.screenshot({path: './screenshots/post/login.png'})
+        await page.screenshot({path: './screenshots/post/login/step5.png'})
     })
     
     test('create post and verify successful creation', async ({ page }) => {
         const url: string = 'http://localhost:3002/ghost/#/';
         //Create post
-        await page.goto(`${url}posts`);   
+        await page.goto(`${url}posts`); 
+        await new Promise(r => setTimeout(r, 1000));
+        await page.screenshot({path: './screenshots/post/creation/step1.png'})  
         await page.goto(`${url}editor/post`)
+        await page.screenshot({path: './screenshots/post/creation/step2.png'})
         const fakeTitle : string = faker.lorem.word();
         await page.getByPlaceholder('Post title').fill(fakeTitle);
+        await page.screenshot({path: './screenshots/post/creation/step3.png'})
         await page.locator('p[data-koenig-dnd-droppable="true"]').fill(fakeTitle);
+        await page.screenshot({path: './screenshots/post/creation/step4.png'})
         await page.getByRole('button', { name: 'Publish', exact: true }).click();
+        await page.screenshot({path: './screenshots/post/creation/step5.png'})
         await new Promise(r => setTimeout(r, 1000));
         await page.locator('button.gh-btn.gh-btn-black.gh-publishmenu-button.gh-btn-icon.ember-view').click();
+        await page.screenshot({path: './screenshots/post/creation/step6.png'})
         await new Promise(r => setTimeout(r, 1000));
         await page.locator('button.gh-btn.gh-btn-black.gh-btn-icon.ember-view').click();
+        await page.screenshot({path: './screenshots/post/creation/step7.png'})
         await new Promise(r => setTimeout(r, 1000));
         await expect(page.getByText(fakeTitle)).toBeVisible();
-        await page.screenshot({path: `./screenshots/post/post${fakeTitle}-created.png`})
+        await page.screenshot({path: './screenshots/post/creation/step8.png'})
         console.log(`----------post ${fakeTitle} created successfully----------`);
 
     })
@@ -42,19 +52,26 @@ test.describe.serial("Posts E2E Scenarios", () => {
     test('edit existing post and verify changes done', async ({ page }) => {
         
         const url: string = 'http://localhost:3002/ghost/#/';
-        await page.goto(`${url}posts`);   
+        await page.goto(`${url}posts`);
+        await new Promise(r => setTimeout(r, 1000));   
+        await page.screenshot({path: './screenshots/post/edition/step1.png'})
         await page.locator('h3.gh-content-entry-title').first().click();
+        await new Promise(r => setTimeout(r, 1000));
+        await page.screenshot({path: './screenshots/post/edition/step2.png'})
         await new Promise(r => setTimeout(r, 2000));
         const initialFakeTitle : string = faker.lorem.word();
         await page.getByPlaceholder('Post title').fill(initialFakeTitle);
+        await page.screenshot({path: './screenshots/post/edition/step3.png'})
         await new Promise(r => setTimeout(r, 2000));
         await page.getByText('Update').first().click();
+        await page.screenshot({path: './screenshots/post/edition/step4.png'})
         await new Promise(r => setTimeout(r, 2000));
         await page.locator('button.gh-btn.gh-btn-black.gh-publishmenu-button.gh-btn-icon.ember-view').click();
-        await page.goto(`${url}posts`);   
+        await page.screenshot({path: './screenshots/post/edition/step5.png'})
+        await page.goto(`${url}posts`);
         await new Promise(r => setTimeout(r, 1000));
         await expect(page.getByText(initialFakeTitle)).toBeVisible();
-        await page.screenshot({path: `./screenshots/post/post${initialFakeTitle}-edited.png`})        
+        await page.screenshot({path: './screenshots/post/edition/step6.png'})
         console.log(`----------post ${initialFakeTitle} edited successfully----------`);
         
           
@@ -65,15 +82,22 @@ test.describe.serial("Posts E2E Scenarios", () => {
         const url: string = 'http://localhost:3002/ghost/#/';
         
         await page.goto(`${url}posts`);
+        await new Promise(r => setTimeout(r, 1000));
+        await page.screenshot({path: './screenshots/post/deletion/step1.png'})
         await page.locator('h3.gh-content-entry-title').first().click();
+        await new Promise(r => setTimeout(r, 1000));
+        await page.screenshot({path: './screenshots/post/deletion/step2.png'})
         await new Promise(r => setTimeout(r, 2000));
         await page.getByTitle('Settings').click();
+        await page.screenshot({path: './screenshots/post/deletion/step3.png'})
         await page.getByRole('button', { name: 'Delete post'}).click();
+        await page.screenshot({path: './screenshots/post/deletion/step4.png'})
         await new Promise(r => setTimeout(r, 1000));
         await page.getByRole('button', { name: 'Delete', exact: true }).click();
+        await page.screenshot({path: './screenshots/post/deletion/step5.png'})
         await new Promise(r => setTimeout(r, 1000));
         await expect(page.url()).toEqual(`${url}posts`);
-        await page.screenshot({path: `./screenshots/post/post-deleted.png`})
+        await page.screenshot({path: './screenshots/post/deletion/step6.png'})
         console.log(`----------post deleted successfully----------`);
     })
     
